50cc1a478b44736fea58868124e76c9eb7755816
mateuslopes92 <mateuslopes92@gmail.com>
2019-10-18 00:24:11 -0300

    Create user model, and controllers of user and session autentication

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Teste!' }));
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
