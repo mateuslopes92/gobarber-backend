f66f94ea1b8a85e3dd5cd8113cd02741228f76c0
mateuslopes92 <mateuslopes92@gmail.com>
2019-11-03 21:39:21 -0300

    Create controllers for appointment, provider and schedule. Create mongodb connection

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,6 +22,11 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
